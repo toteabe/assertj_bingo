0d82771cd59300c8cec04f443ce27459ecb3236a
toteabe <toteabe@yahoo.es>
2024-04-02 21:52:48 +0200

    test de prueba funcionamiento de assertj

---
+++ src/test/java/org/iesvdm/UtilesArraysTest.java
@@ -0,0 +1,12 @@
+package org.iesvdm;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UtilesArraysTest {
+
+    @Test
+    void prueba() {
+        Assertions.assertFalse("hola" == new String("hola"));
+    }
+}
