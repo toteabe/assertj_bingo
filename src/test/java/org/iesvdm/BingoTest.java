e02f6b0acc0cd072df6013c7171b3daf5c838539
toteabe <toteabe@yahoo.es>
2024-04-02 21:44:56 +0200

    test de prueba funcionamiento de assertj

---
+++ src/test/java/org/iesvdm/BingoTest.java
@@ -0,0 +1,14 @@
+package org.iesvdm;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class BingoTest {
+
+    @Test
+    void testPrueba() {
+        assertThat(1!=1).isTrue();
+    }
+
+}
