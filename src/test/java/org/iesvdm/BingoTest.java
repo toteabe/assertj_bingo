2096c01ff9417a4f36e234c7a4f1c97e4e66bd4b
toteabe <toteabe@yahoo.es>
2024-04-03 22:17:12 +0200

    otro cambio

--- src/test/java/org/iesvdm/BingoTest.java
+++ src/test/java/org/iesvdm/BingoTest.java
@@ -2,6 +2,8 @@ package org.iesvdm;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class BingoTest {
@@ -10,5 +12,42 @@ public class BingoTest {
     void testPrueba() {
         assertThat(1!=1).isTrue();
     }
+
+
+    @Test
+    void testPonerBlancos(){
+        // Genero los arrays columnas:
+//        int[] col0 = new int[3];
+//        int[] col1 = new int[3];
+//        int[] col2 = new int[3];
+//        int[] col3 = new int[3];
+//        int[] col4 = new int[3];
+//        int[] col5 = new int[3];
+//        int[] col6 = new int[3];
+//        int[] col7 = new int[3];
+//        int[] col8 = new int[3];
+//
+//        // Los paso a mi matriz o array bidimensional:
+//        int[][] carton = {col0, col1, col2, col3, col4, col5, col6, col7, col8};
+
+        int[][] cartonDirecto = new int[9][3];
+
+        Bingo.ponerBlancos(cartonDirecto);
+        String[] filas = {"", "", ""};
+        for( int[] col : cartonDirecto ) {
+            for(int j = 0; j < 3; j++) {
+                filas[j] += (col[j] == 0 ? " 0": col[j]) +" ";
+            }
+        }
+
+        for(String fila : filas)
+            System.out.println(fila);
+
+
+
+
+
+    }
+
 
 }
