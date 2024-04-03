5aeba1d2af167ea62598a6048b51a6ecd15db5cc
luxemalo <luchemalo69@gmail.com>
2024-04-03 12:36:51 +0200

    practica2-3abril

--- src/test/java/org.iesvdm/BingoTest.java
+++ src/test/java/org.iesvdm/BingoTest.java
@@ -35,7 +35,7 @@ public class BingoTest {
             int[] col7=new int[3];
             int[] col8=new int[3];
 
-
+//
             int[][] carton={col0,col1,col2,col3,col4,col5,col6,col7,col8};
 
             //for(int[]col:carton)
